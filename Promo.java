d0297b371f2f33dccc6a7e969f5dde8fd26d5366
Charlene Manning <113045523+dyingshawarma@users.noreply.github.com>
2024-05-11 00:18:12 +0800

    Update and rename Laboratory l SW 4 Visitor Pattern to Promo.java

---
+++ Promo.java
@@ -0,0 +1,4 @@
+public abstract class Promo {
+    public abstract String showAllowance(Telco telco, double promoPrice);
+    public abstract String showUnliCallsTextOffer(Telco telco, boolean unliCallText);
+}
